5c10f981eb70dd6ed54d9259304b2194e02c7cb6
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2023-02-13 17:45:29 +0700

    Oo
    Signed-off-by: Sip - userbot  <110762610+sip-userbot@users.noreply.github.com>

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ API_HASH = getenv("API_HASH")
 API_ID = int(getenv("API_ID", ""))
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001867019433]
+    BLACKLIST_CHAT = [-1001808819173]
 BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "1.1.5@main"
