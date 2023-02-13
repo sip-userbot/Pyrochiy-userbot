0824e3418803251b611279ff73f863bd5b364ebd
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2023-02-13 17:52:20 +0700

    Yyyu
    Signed-off-by: Sip - userbot  <110762610+sip-userbot@users.noreply.github.com>

--- Pyrochiy/modules/help.py
+++ Pyrochiy/modules/help.py
@@ -44,7 +44,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @obrolansuar × @Karc0de •",
+                f"```{str(ac)}```\n• @ShicyyXCode × @ShicyxC0d •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Karc0de"
+            this_command += "© @ShicyxC0d"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @obrolansuar × @Karc0de •"
+            message, f"```{str(ac)}```\n• @ShicyyXCode × @ShicyxC0d •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Karc0de"
+            this_command += "© @ShicyxC0d"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
